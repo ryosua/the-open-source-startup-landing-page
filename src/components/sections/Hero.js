986037b7320d9e02756a56e6559e9626878b6458
Ryan <hi@ryanyosua.me>
2020-06-06 15:21:00 -0600

    prettier

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -1,81 +1,81 @@
-import React from 'react';
-import classNames from 'classnames';
-import { SectionProps } from '../../utils/SectionProps';
-import ButtonGroup from '../elements/ButtonGroup';
-import Button from '../elements/Button';
+import React from 'react'
+import classNames from 'classnames'
+import { SectionProps } from '../../utils/SectionProps'
+import ButtonGroup from '../elements/ButtonGroup'
+import Button from '../elements/Button'
 
 const propTypes = {
-  ...SectionProps.types
+    ...SectionProps.types
 }
 
 const defaultProps = {
-  ...SectionProps.defaults
+    ...SectionProps.defaults
 }
 
 const Hero = ({
-  className,
-  topOuterDivider,
-  bottomOuterDivider,
-  topDivider,
-  bottomDivider,
-  hasBgColor,
-  invertColor,
-  ...props
+    className,
+    topOuterDivider,
+    bottomOuterDivider,
+    topDivider,
+    bottomDivider,
+    hasBgColor,
+    invertColor,
+    ...props
 }) => {
+    const outerClasses = classNames(
+        'hero section center-content',
+        topOuterDivider && 'has-top-divider',
+        bottomOuterDivider && 'has-bottom-divider',
+        hasBgColor && 'has-bg-color',
+        invertColor && 'invert-color',
+        className
+    )
 
+    const innerClasses = classNames(
+        'hero-inner section-inner',
+        topDivider && 'has-top-divider',
+        bottomDivider && 'has-bottom-divider'
+    )
 
-  
-
-  const outerClasses = classNames(
-    'hero section center-content',
-    topOuterDivider && 'has-top-divider',
-    bottomOuterDivider && 'has-bottom-divider',
-    hasBgColor && 'has-bg-color',
-    invertColor && 'invert-color',
-    className
-  );
-
-  const innerClasses = classNames(
-    'hero-inner section-inner',
-    topDivider && 'has-top-divider',
-    bottomDivider && 'has-bottom-divider'
-  );
-
-  return (
-    <section
-      {...props}
-      className={outerClasses}
-    >
-      <div className="container-sm">
-        <div className={innerClasses}>
-          <div className="hero-content">
-            <h1 className="mt-0 mb-16 reveal-from-bottom" data-reveal-delay="200">
-              The Open Source <span className="text-color-primary">Startup</span>
-            </h1>
-            <div className="container-xs">
-              <p className="m-0 mb-32 reveal-from-bottom" data-reveal-delay="400">
-              Learn to code and build startups.
-                </p>
-              <div className="reveal-from-bottom" data-reveal-delay="600">
-                <ButtonGroup>
-                  <Button tag="a" color="primary" wideMobile href="https://www.youtube.com/channel/UClqbavbIJWxl-iMBpHb37JA/featured">
-                    Start Watching
-                    </Button>
-                  <Button tag="a" color="dark" wideMobile href="https://github.com/the-open-source-startup">
-                    View on Github
-                    </Button>
-                </ButtonGroup>
-              </div>
+    return (
+        <section {...props} className={outerClasses}>
+            <div className="container-sm">
+                <div className={innerClasses}>
+                    <div className="hero-content">
+                        <h1 className="mt-0 mb-16 reveal-from-bottom" data-reveal-delay="200">
+                            The Open Source <span className="text-color-primary">Startup</span>
+                        </h1>
+                        <div className="container-xs">
+                            <p className="m-0 mb-32 reveal-from-bottom" data-reveal-delay="400">
+                                Learn to code and build startups.
+                            </p>
+                            <div className="reveal-from-bottom" data-reveal-delay="600">
+                                <ButtonGroup>
+                                    <Button
+                                        tag="a"
+                                        color="primary"
+                                        wideMobile
+                                        href="https://www.youtube.com/channel/UClqbavbIJWxl-iMBpHb37JA/featured">
+                                        Start Watching
+                                    </Button>
+                                    <Button
+                                        tag="a"
+                                        color="dark"
+                                        wideMobile
+                                        href="https://github.com/the-open-source-startup">
+                                        View on Github
+                                    </Button>
+                                </ButtonGroup>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-        
-        </div>
-      </div>
-    </section>
-  );
+        </section>
+    )
 }
 
-Hero.propTypes = propTypes;
-Hero.defaultProps = defaultProps;
+Hero.propTypes = propTypes
+Hero.defaultProps = defaultProps
 
-export default Hero;
+export default Hero
