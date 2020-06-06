2383cd5848dfe2e3f5ff3e7bb611473c4fbb00ce
Ryan <hi@ryanyosua.me>
2020-06-06 15:37:47 -0600

    changes to design

--- src/layouts/LayoutDefault.js
+++ src/layouts/LayoutDefault.js
@@ -1,15 +1,11 @@
-import React from 'react';
-import Header from '../components/layout/Header';
-import Footer from '../components/layout/Footer';
+import React from 'react'
+import Header from '../components/layout/Header'
 
 const LayoutDefault = ({ children }) => (
-  <>
-    <Header navPosition="right" className="reveal-from-bottom" />
-    <main className="site-content">
-      {children}
-    </main>
-    <Footer />
-  </>
-);
+    <>
+        <Header navPosition="right" className="reveal-from-bottom" />
+        <main className="site-content">{children}</main>
+    </>
+)
 
-export default LayoutDefault;  
+export default LayoutDefault
